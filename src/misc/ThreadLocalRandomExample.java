8f03fa9d963d0c46411168554f3c9964e329b808
Ajay Yadava <ajayyadava@apache.org>
2017-01-03 18:25:39 -0500

    Illustrate usage of ThreadLocalRandom for generating random numbers in concurrent applications.

---
+++ src/misc/ThreadLocalRandomExample.java
@@ -0,0 +1,67 @@
+package misc;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+/**
+
+ Use ThreadLocalRandom Instead of Random for generating random numbers
+
+
+ Notes:
+
+ IntStream.range(0,10)
+ IntStream.rangeClosed(0,10)
+
+ ## Expression lambda
+ .forEach(i -> new Thread(new Task("Thread"+i)).start());
+
+ ## Statement Lambda
+ .forEach(i -> {
+    .......
+    .......
+    .......
+ });
+
+
+ */
+public class ThreadLocalRandomExample {
+
+    private static final class Task implements Runnable {
+        private final String name;
+
+        Task(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+
+            // Just a fancy way of saying for i= 1; i <= 10; i++
+            // range with end included
+            IntStream.rangeClosed(1, 2)
+                    // lambda with {..} is called statement lambda
+                    .forEach(i -> {
+                        String prefix = name + "-" + i;
+
+                        // randomNumber [0, 10). Upper Bound included.
+                        int randomNumber = ThreadLocalRandom.current().nextInt(10);
+                        System.out.println(prefix + " Excluded[1, 10) " + randomNumber);
+
+                        // randomNumber [1, 10). Upper Bound is exclusive
+                        randomNumber = ThreadLocalRandom.current().nextInt(1, 10);
+                        System.out.println(prefix + " Included[1, 10] " + randomNumber);
+
+                    });
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // range with end Exclusive
+        IntStream.range(0, 2)
+                // example of expression lambda
+                .forEach(i -> new Thread(new Task("Thread"+i)).start());
+
+    }
+}
