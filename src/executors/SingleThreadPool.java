d12b04c4f7748d9ea8814a4d66fc2561af93da87
Ajay Yadava <ajayyadava@apache.org>
2017-01-05 17:49:04 -0500

    Executors: Single thread executor

--- src/executors/SingleThreadPool.java
+++ src/executors/SingleThreadPool.java
@@ -5,7 +5,6 @@ import java.util.concurrent.*;
 public class SingleThreadPool {
 
     private static final class Task implements Runnable {
-
         private final String name;
 
         Task(String name) {
@@ -22,12 +21,14 @@ public class SingleThreadPool {
             System.out.printf("Task %s has finished.\n", name);
         }
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         // NOTE: Since this is a single thread executor it doesn't return a threadpoolExecutor instance
-        Executor executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
         for (int i = 0; i < 10; i++) {
             executor.execute(new Task(String.valueOf(i)));
-
         }
+        TimeUnit.MILLISECONDS.sleep(500);
+        executor.shutdown();
     }
 }
